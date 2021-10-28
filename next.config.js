dd634c867802f62ec22bbacff179f69d218ce0d5
rh5rep <rh5rami10@gmail.com>
2021-10-28 11:34:03 -0400

    ci(Netlify): fix deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  distDir: "out",
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/index",
-        permanent: true,
-      },
-    ];
-  },
 };
